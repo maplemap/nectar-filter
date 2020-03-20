9904f98bc270542c9655f48082a091f060717a6e
Serg Illarionov <s.illarionov@spd-ukraine.com>
2020-03-20 21:29:13 +0200

    setup: environment

---
+++ src/app/app.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import {Button} from 'antd';
+
+import './app.css';
+
+export const App = () => {
+  return (
+    <div className="App">
+      <Button type="primary">Button</Button>
+    </div>
+  );
+};
+
