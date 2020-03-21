63903fe7b3fa4d6db41391de610186c0cfe937aa
Serg Illarionov <s.illarionov@spd-ukraine.com>
2020-03-21 16:34:52 +0200

    update: add new theme color for app style

--- src/app/app.js
+++ src/app/app.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import {Layout} from 'antd';
-import logo from '../logo.png';
+import {FilterMenu} from '../components/filter-menu';
+import filtersData from '../data';
 
+import logo from '../logo.png';
 import './app.less';
 
 export const App = () => {
     const {Header, Content} = Layout;
+    const filters = Object.keys(filtersData);
 
     return (
         <Layout className="nectar-app">
@@ -14,7 +17,9 @@ export const App = () => {
                     <img src={logo} alt="nectar-filter-logo"/>
                 </div>
             </Header>
-            <Header/>
+            <Header className="nectar-app__sub-header">
+                <FilterMenu filters={filters}/>
+            </Header>
             <Content className="nectar-app__content-box">
                 <div className="nectar-app__content">Content</div>
             </Content>
