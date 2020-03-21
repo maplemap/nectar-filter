a66fd35dd97387b224e3092757e54ff1b92e02e6
Serg Illarionov <s.illarionov@spd-ukraine.com>
2020-03-21 15:22:45 +0200

    add: less support

--- config-overrides.js
+++ config-overrides.js
@@ -1,10 +1,16 @@
 // eslint-disable-next-line import/no-extraneous-dependencies
-const {override, fixBabelImports} = require('customize-cra');
+const {override, fixBabelImports, addLessLoader} = require('customize-cra');
 
 module.exports = override(
     fixBabelImports('import', {
         libraryName: 'antd',
         libraryDirectory: 'es',
-        style: 'css',
+        style: true,
     }),
+    addLessLoader({
+        javascriptEnabled: true,
+        modifyVars: {
+            '@primary-color': '#1DA57A'
+        },
+    })
 );
