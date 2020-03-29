6d0e49b2d2bd0f180aa7aee559dfb408ffde3234
Serg Illarionov <s.illarionov@spd-ukraine.com>
2020-03-29 20:49:05 +0300

    Updates

---
+++ precache-manifest.421deb7619e5573b9ad450b479b5283e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "837a248e841905d6fb33fa8d392635dc",
+    "url": "/nectar-filter/index.html"
+  },
+  {
+    "revision": "679cbb5696efe6d66ea6",
+    "url": "/nectar-filter/static/css/2.1987e178.chunk.css"
+  },
+  {
+    "revision": "ec9aa1a2d321ef26fcd6",
+    "url": "/nectar-filter/static/css/main.399000e4.chunk.css"
+  },
+  {
+    "revision": "679cbb5696efe6d66ea6",
+    "url": "/nectar-filter/static/js/2.2846b22b.chunk.js"
+  },
+  {
+    "revision": "fdd30b2ab44ae896589b798d411d5c2c",
+    "url": "/nectar-filter/static/js/2.2846b22b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ec9aa1a2d321ef26fcd6",
+    "url": "/nectar-filter/static/js/main.16ae252c.chunk.js"
+  },
+  {
+    "revision": "6544d6e7aade40c6add7",
+    "url": "/nectar-filter/static/js/runtime-main.1fa3df40.js"
+  }
+]);
